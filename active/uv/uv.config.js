6b79ef592680932c3a5ad0eb9a2f7e560962c6d3
zaka13alt <zakariyaliban719@gmail.com>
2026-07-15 13:38:36 -0500

    Fix URL in uv.config.js for API endpoint

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://tompbare-1.onrender.com/api/b',
+    bare:'https://tompbare-1.onrender.com/api/b/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
